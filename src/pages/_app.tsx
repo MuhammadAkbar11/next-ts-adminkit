507e5860d129c3ae47334499f9c6b224283f7d7c
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2023-03-21 21:03:06 +0700

    feat: compose context provider for handle multiple context provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,54 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable react/jsx-no-useless-fragment */
+import '@/styles/app.scss';
+import React from 'react';
+import { NextPage } from 'next';
 import type { AppProps } from 'next/app';
+import { SSRProvider } from 'react-bootstrap';
+import Head from 'next/head';
+import ComposeCtxProvider from '@utils/context';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+type NextPageComponentProps = NextPage & {
+  provider?: React.ComponentType;
+  providers?: React.ComponentType[];
+  layout?: React.ComponentType;
+};
+
+type AppPropsWrapp = AppProps & {
+  Component: NextPageComponentProps;
+};
+
+export default function App({ Component, pageProps }: AppPropsWrapp) {
+  const Layout =
+    Component.layout ||
+    (({ children }: { children: React.ReactNode }) => <>{children}</>);
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <meta
+          name="description"
+          content="Responsive Admin & Dashboard Template based on Bootstrap 5"
+        />
+        <meta name="author" content="AdminKit" />
+        <meta
+          name="keywords"
+          content="adminkit, bootstrap, bootstrap 5, admin, dashboard, template, responsive, css, sass, html, theme, front-end, ui kit, web"
+        />
+        <title>AdminKit Demo - Bootstrap 5 Admin Template</title>
+      </Head>
+      <SSRProvider>
+        <ComposeCtxProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>{' '}
+        </ComposeCtxProvider>
+      </SSRProvider>
+    </>
+  );
 }
