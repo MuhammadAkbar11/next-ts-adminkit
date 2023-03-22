da86816c91e6f38b21c904053c986e5d48736168
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2023-03-22 22:45:08 +0700

    feat: Dynamic react-feather component

---
+++ src/components/Icons/DynamicFeatherIcon.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import * as RFIcon from 'react-feather';
+import { FeatherIconsTypes } from '@utils/types';
+
+type Props = {
+  name: FeatherIconsTypes;
+} & RFIcon.IconProps;
+
+function DynamicFeatherIcon({ name, ...props }: Props) {
+  const Icon = RFIcon[
+    `${name}` as FeatherIconsTypes
+  ] as React.ElementType<RFIcon.IconProps>;
+
+  return <Icon {...props} />;
+}
+
+export default DynamicFeatherIcon;
