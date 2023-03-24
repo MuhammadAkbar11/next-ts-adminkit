8190bdfb36df8d757acf6181962a123fd2b129be
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2023-03-24 20:28:30 +0700

    refactor: sidebar CTA component

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -3,10 +3,12 @@ import React from 'react';
 import SimpleBar from 'simplebar-react';
 import clsx from 'classnames';
 import { useAdminLayoutContext } from '@utils/context/AdminLayoutContext';
+import navigationConfigs from '@configs/navigationConfigs';
 import SidebarBrand from './SidebarBrand';
 import SidebarHeader from './SidebarHeader';
 import SidebarItem from './SidebarItem';
 import SidebarItemCollapse from './SidebarItemCollapse';
+import SidebarCta from './SidebarCta';
 
 type Props = {};
 
@@ -23,63 +25,45 @@ export default function Sidebar({}: Props) {
   const sidebarClassName = clsx('sidebar', {
     collapsed: openSidebar,
   });
+
   return (
     <nav id="sidebar" className={sidebarClassName}>
       <SimpleBar
         scrollableNodeProps={{ ref: scrollableNodeRef }}
-        // forceVisible="y"
         className="sidebar-content border-dark  "
       >
         <SidebarBrand />
         <ul className="sidebar-nav">
-          <SidebarHeader title="Pages" />
-          <SidebarItem name="Dashboard" icon="Sliders" href="/" />
-          <SidebarItem name="Profile" icon="User" href="/pages-profile" />
-          <SidebarItemCollapse
-            name="Auth"
-            icon="Users"
-            links={[
-              {
-                name: 'Sign In',
-                href: '/pages-sign-in',
-              },
-              {
-                name: 'Sign Up',
-                href: '/pages-sign-up',
-              },
-            ]}
-          />
-          <SidebarItem name="Blank" icon="Book" href="/pages-blank" />
-          {/* Tools & Components */}
-          <SidebarHeader title="Tools & Components" />
-          <SidebarItem name="Buttons" icon="Square" href="/ui-buttons" />
-          <SidebarItem name="Forms" icon="CheckSquare" href="/ui-forms" />
-          <SidebarItem name="Cards" icon="Grid" href="/ui-cards" />
-          <SidebarItem
-            name="Typography"
-            icon="AlignLeft"
-            href="/ui-typography"
-          />
-          <SidebarItem name="Icons" icon="Coffee" href="/ui-feather" />
-          {/* Plugins & Addons */}
-          <SidebarHeader title="Plugins & Addons" />
-          <SidebarItem name="Charts" icon="BarChart2" href="/ui-feather" />
-          <SidebarItem name="Maps" icon="Map" href="/ui-google" />
+          {Object.keys(navigationConfigs).map((key) => {
+            return (
+              <React.Fragment key={key}>
+                <SidebarHeader title={navigationConfigs[key].title} />
+                {navigationConfigs[key].navItems.map((nv, idx) => {
+                  const navKey = idx;
+                  if (nv?.navSubItems) {
+                    return (
+                      <SidebarItemCollapse
+                        key={navKey}
+                        name={nv.name}
+                        icon={nv.icon}
+                        links={nv.navSubItems}
+                      />
+                    );
+                  }
+                  return (
+                    <SidebarItem
+                      name={nv.name}
+                      icon={nv.icon}
+                      href={nv?.href ?? '#/'}
+                      key={navKey}
+                    />
+                  );
+                })}
+              </React.Fragment>
+            );
+          })}
         </ul>
-        <div className="sidebar-cta">
-          <div className="sidebar-cta-content">
-            <strong className="d-inline-block mb-2">Upgrade to Pro</strong>
-            <div className="mb-3 text-sm">
-              Are you looking for more components? Check out our premium
-              version.
-            </div>
-            <div className="d-grid">
-              <a href="upgrade-to-pro.html" className="btn btn-primary">
-                Upgrade to Pro
-              </a>
-            </div>
-          </div>
-        </div>
+        <SidebarCta />
       </SimpleBar>
     </nav>
   );
